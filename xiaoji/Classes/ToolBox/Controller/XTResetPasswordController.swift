d7ba7820e4bef7dc5409a4340f35c359797f2955
dingpuyu <xiaotei@xiaoteisdeMacBook-Pro.local>
2016-04-24 17:39:40 +0800

    用户信息

---
+++ xiaoji/Classes/ToolBox/Controller/XTResetPasswordController.swift
@@ -0,0 +1,16 @@
+//
+//  XTResetPasswordController.swift
+//  xiaoji
+//
+//  Created by xiaotei's MacBookPro on 16/4/16.
+//  Copyright © 2016年 xiaotei's MacBookPro. All rights reserved.
+//
+
+import Foundation
+
+class XTResetPasswordController: UIViewController {
+    
+
+    
+    
+}
